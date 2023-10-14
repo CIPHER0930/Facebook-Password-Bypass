64f5982a87fe32223f739d907fd16746e4386611
SHIELD660 <richmondrichmond183@gmail.com>
2023-10-14 13:11:07 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "
+d88888b  .d8b.   .o88b. d88888b d8888b.  .d88b.   .d88b.  db   dD      db   db  .d8b.   .o88b. db   dD d888888b d8b   db  d888b       d888888b  .d88b.   .d88b.  db      
+88'     d8' `8b d8P  Y8 88'     88  `8D .8P  Y8. .8P  Y8. 88 ,8P'      88   88 d8' `8b d8P  Y8 88 ,8P'   `88'   888o  88 88' Y8b      `~~88~~' .8P  Y8. .8P  Y8. 88      
+88ooo   88ooo88 8P      88ooooo 88oooY' 88    88 88    88 88,8P        88ooo88 88ooo88 8P      88,8P      88    88V8o 88 88              88    88    88 88    88 88      
+88~~~   88~~~88 8b      88~~~~~ 88~~~b. 88    88 88    88 88`8b        88~~~88 88~~~88 8b      88`8b      88    88 V8o88 88  ooo         88    88    88 88    88 88      
+88      88   88 Y8b  d8 88.     88   8D `8b  d8' `8b  d8' 88 `88.      88   88 88   88 Y8b  d8 88 `88.   .88.   88  V888 88. ~8~         88    `8b  d8' `8b  d8' 88booo. 
+YP      YP   YP  `Y88P' Y88888P Y8888P'  `Y88P'   `Y88P'  YP   YD      YP   YP YP   YP  `Y88P' YP   YD Y888888P VP   V8P  Y888P          YP     `Y88P'   `Y88P'  Y88888P 
+                                                                                                                                                                         
+                                                                                                                                                                         
+"
 # This script recovers a Facebook password using a cracked OTP and changes the password.
 
 # USAGE:
